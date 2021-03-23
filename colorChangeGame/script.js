991adcc331da8c1d219887e523f38e935377db90
Arichson <arichsonh@gmail.com>
2021-03-23 08:17:28 -0700

    fixed some issues, need to make colors spread out more

--- colorChangeGame/script.js
+++ colorChangeGame/script.js
@@ -15,7 +15,7 @@
 const theFirstBoard = document.querySelector("#boardOne");
 const startButton = document.querySelector("#start");
 const openingModal = document.querySelector("#opening");
-const colorChooseModal = document.querySelector("#colorChoose")
+const colorChooseModal = document.querySelector("#colorChoose");
 const redButton = document.querySelector("#redButton");
 const yellowButton = document.querySelector("#yellowButton");
 const greenButton = document.querySelector("#greenButton");
@@ -28,11 +28,11 @@ const greyButton = document.querySelector("#greyButton");
 
 
 ///////////Variables
-const theColorsAvailable = ["red", "yellow", "green", "blue", "cyan", "purple","grey"]
+const theColorsAvailable = ["red", "yellow", "green", "blue", "cyan", "purple","grey"];
 const firstBoardArray = [];
 const secondBoardArray = [];
 const thirdBoardArray = [];
-const players = ["player", "enemy"]
+const players = ["player", "enemy"];
 const playerCurrentColor = theColorsAvailable[0];
 const enemyCurrentColor = theColorsAvailable[6];
 
@@ -44,13 +44,13 @@ const playersTurns = () => {
     const currentTurn = turns % 2;
     turns += 1;
     if( currentTurn === 0) {
-        return players[0]
+        return players[0];
     } else {
-        return players[1]
+        return players[1];
     }
 }
 const randNum = (limit) => {
-    return Math.floor(Math.random() * limit)
+    return Math.floor(Math.random() * limit);
 }
 const firstBoardMaker = () => {
     let pieceColor = "";
@@ -58,33 +58,26 @@ const firstBoardMaker = () => {
     for(let i = 0; i < 10; i++) {
         const colParity = () => {
             if (i % 2 === 0){
-                return "colEven"
+                return "colEven";
             } else {
-                return "colOdd"
+                return "colOdd";
             }
         }
-        let character = "";
         const colArray = [];
         pieceColor += `<div id=board1Col${i} class="${colParity(i)} column">`;
         for(let j = 0; j<10; j++){
-            if(i===0 && j === 9){
-                character = players[0];
-            } else if (i === 9 && j === 0){
-                character = players[1];
-            } else {
-                character = "";
-            }
             const owner = () =>{
-                if(i===0 && j === 9){
-                    return players[0]
+                if(i===8 && j === 4){
+                    return players[0];
                 } else if (i === 9 && j === 0){
                     return players[1];
                 } else {
                     return "none";
                 }
             }
-            const currentColor = theColorsAvailable[randNum(theColorsAvailable.length)];
-            const newDiv = `<div id="piece${j}${i}" class="${currentColor} theHexagons ${character}"></div>`;
+            const currentColor =
+            theColorsAvailable[randNum(theColorsAvailable.length)];
+            const newDiv = `<div id="piece${j}${i}" class="${currentColor} theHexagons ${owner()}"></div>`;
             pieceColor += newDiv;
             colArray.push({
                 color: currentColor,
@@ -107,13 +100,13 @@ firstBoardMaker();
 // console.log(firstBoardArray[11])
 
 const closeTheOpening = () => {
-    openingModal.classList.add("closeModal")
+    openingModal.classList.add("closeModal");
     colorChooseModal.classList.remove("closeModal");
 }
 const closeColorChoose = () => {
     // colorChooseModal.classList.add("closeModal");
 }
-console.log(playersTurns())
+// console.log(playersTurns())
 
 const checkHexes = () => {
     // for(let i = 0; i < 10; i++){
@@ -124,7 +117,7 @@ const checkHexes = () => {
     //     }
     // }
 }
-
+/*
 const gameFunction = (colorChosen) => {
     /////fix later
     
@@ -168,38 +161,134 @@ const gameFunction = (colorChosen) => {
 
     /////after this function elds, it turns to the enemy players turn
 }
-
-/*
-const gameFunction = (colorChosen) => {
-// Check each column then row if its owned by player 
-    for(let i = 0; i < 10; i++){
-        for(let j = 0; j < 10; j++){
-            let col = i;
-            let row = j;
-            const currentRowCol = document.querySelector(`#piece${j}${i}`)
-            // const firstBoardRowCol = document.querySelector(`#piece${j}${i}`)
-
-            const rowAbove = document.querySelector(`#piece${j-1}${i}`)
-            const rowBelow = document.querySelector(`#piece${j+1}${i}`)
-            //check for class player
-            for( let z = 0; z < 5; z++){}
-            if(currentRowCol.classList.contains(`player`)){
-                // check the row above and below if it has the current color
-                if(rowAbove.classList.contains(currentColor)){
-                    rowAbove.classList.add("player");
-                    row-=1;
+*/
+const removeColor = (theTarget) => {
+    theColorsAvailable.forEach( color => {
+        theTarget.classList.remove(color);
+    })
+}
+const changePlayerColor = (currentColor) => {
+    firstBoardArray.forEach( array =>{
+        array.forEach(object => {
+            if( object.owner === "player"){
+                object.color = currentColor;
+                const rowColTarget = document.querySelectorAll(`.player`);
+                // console.log(rowColTarget.length);
+                for( let i = 0; i < rowColTarget.length; i++){
+                    removeColor(rowColTarget[i]);
+                    rowColTarget[i].classList.add(currentColor);
+                    // console.log(rowColTarget[i].classList.contains('player'));
                 }
-                if(rowBelow.classList.contains(currentColor)){
-                    rowBelow.classList.add("player");
-                    row+=1;
-                }
-
-                ////////so far this just checks one column? 
-                ///////need to check the rows next to the columns too
+            }
+        })
+    })
+}
+const checkAbove = (j, i, currentColor) => {
+    const rowAbove = j-1;
+    const rowAboveChecked = document.querySelector(`#piece${rowAbove}${i}`);
+    const currentRowCol = document.querySelector(`#piece${j}${i}`);
+    if(currentRowCol.classList.contains(`player`)){
+        if(rowAboveChecked.classList.contains(currentColor) && (rowAbove >= 0 && rowAbove<=9)/*&& rowAboveChecked.classList.contains('none')*/){
+            rowAboveChecked.classList.remove('none');
+            rowAboveChecked.classList.add("player");
+        }
+    }
+}
+const checkBelow = (j, i, currentColor) => {
+    const rowBelow = j+1;
+    const rowBelowChecked = document.querySelector(`#piece${rowBelow}${i}`);
+    const currentRowCol = document.querySelector(`#piece${j}${i}`);
+    if(currentRowCol.classList.contains(`player`)){
+        if(rowBelowChecked.classList.contains(currentColor) && (rowBelow <= 9 && rowBelow >= 0)/*&& rowBelowChecked.classList.contains('none')*/){
+            rowBelowChecked.classList.remove('none');
+            rowBelowChecked.classList.add("player");
+        }
+    }
+}
+const checkLeft = (j, i, currentColor) => {
+    const sameRow = j;
+    const rowBelow = j+1;
+    const rowAbove = j - 1;
+    const currentRowCol = document.querySelector(`#piece${j}${i}`);
+    if( i % 2 === 0){
+        const rowBotLeft = document.querySelector(`#piece${rowBelow}${i-1}`);
+        const rowTopLeft = document.querySelector(`#piece${sameRow}${i-1}`);
+        if(currentRowCol.classList.contains(`player`)){
+            if(rowBotLeft.classList.contains(currentColor) && (rowBelow <= 9 && rowBelow >= 0)/*&& rowBotLeft.classList.contains('none')*/){
+                rowBotLeft.classList.remove('none');
+                rowBotLeft.classList.add("player");
+            }
+            if(rowTopLeft.classList.contains(currentColor) && (rowBelow <= 9 && rowBelow >= 0)/*&& rowTopLeft.classList.contains('none')*/){
+                rowTopLeft.classList.remove('none');
+                rowTopLeft.classList.add("player");
+            }
+        }
+    } else {
+        const rowBotLeft = document.querySelector(`#piece${sameRow}${i-1}`);
+        const rowTopLeft = document.querySelector(`#piece${rowAbove}${i-1}`);
+        if(currentRowCol.classList.contains(`player`)){
+            if(rowBotLeft.classList.contains(currentColor) && (rowBelow < 9 && rowBelow > 0)/*&& rowBotLeft.classList.contains('none')*/){
+                rowBotLeft.classList.remove('none');
+                rowBotLeft.classList.add("player");
+            }
+            if(rowTopLeft.classList.contains(currentColor) && (rowBelow < 9 && rowBelow > 0)/*&& rowTopLeft.classList.contains('none')*/){
+                rowTopLeft.classList.remove('none');
+                rowTopLeft.classList.add("player");
             }
         }
     }
-}*/
+}
+const checkRight = (j, i, currentColor) => {
+    const sameRow = j;
+    const rowBelow = j+1;
+    const rowAbove = j - 1;
+    const currentRowCol = document.querySelector(`#piece${j}${i}`);
+    if( i % 2 === 0){
+        const rowBotRight = document.querySelector(`#piece${rowBelow}${i+1}`);
+        const rowTopRight = document.querySelector(`#piece${sameRow}${i+1}`);
+        if(currentRowCol.classList.contains(`player`)){
+            if(rowBotRight.classList.contains(currentColor) && (rowBelow <= 9 && rowBelow >= 0)/*&& rowBotRight.classList.contains('none')*/){
+                rowBotRight.classList.remove('none');
+                rowBotRight.classList.add("player");
+            }
+            if(rowTopRight.classList.contains(currentColor) && (rowBelow <= 9 && rowBelow >= 0)/*&& rowTopRight.classList.contains('none')*/){
+                rowTopRight.classList.remove('none');
+                rowTopRight.classList.add("player");
+            }
+        }
+    } else {
+        const rowBotRight = document.querySelector(`#piece${sameRow}${i-1}`);
+        const rowTopRight = document.querySelector(`#piece${rowAbove}${i-1}`);
+        if(currentRowCol.classList.contains(`player`)){
+            if(rowBotRight.classList.contains(currentColor) && (rowBelow < 9 && rowBelow > 0)/*&& rowBotRight.classList.contains('none')*/){
+                rowBotRight.classList.remove('none');
+                rowBotRight.classList.add("player");
+            }
+            if(rowTopRight.classList.contains(currentColor) && (rowBelow < 9 && rowBelow > 0)/*&& rowTopRight.classList.contains('none')*/){
+                rowTopRight.classList.remove('none');
+                rowTopRight.classList.add("player");
+            }
+        }
+    }
+}
+const gameFunction = (currentColor) => {
+// Check each column then row if its owned by player 
+    for(let i = 0; i < 10; i++){
+        for(let j = 0; j < 10; j++){
+            if(j>0){
+                checkAbove(j, i, currentColor);}
+            if(j < 9) {
+                checkBelow(j, i, currentColor);}
+            if(i > 0 && j>0 && j < 9){
+                checkLeft(j, i, currentColor)}
+            if(i < 9 && j>0 && j < 9){
+                checkRight(j, i, currentColor)}
+                ////////so far this just checks one column? 
+                ///////need to check the rows next to the columns too
+            changePlayerColor(currentColor);
+        }          
+    }
+}
 
 
 ////////need to somehow limit the color able to choose by current ones played
@@ -208,34 +297,34 @@ const gameFunction = (colorChosen) => {
 //maybe make an array that stores 2 colors at a time and it will close those 2
     //ex. color = [blue, red]
     // push(new color) and remove hidden class from that modal color
-    //shift first index and give the modal a hidden class
+    //shift first index and give the modal a hidden class 
 const chooseRed = () =>{
     gameFunction(theColorsAvailable[0]);
     closeColorChoose();
 }
 const chooseYellow = () =>{
     gameFunction(theColorsAvailable[1]);
-        closeColorChoose();
+    closeColorChoose();
 }
 const chooseGreen = () =>{
     gameFunction(theColorsAvailable[2]);
-        closeColorChoose();
+    closeColorChoose();
 }
 const chooseBlue = () =>{
     gameFunction(theColorsAvailable[3]);
-        closeColorChoose();
+    closeColorChoose();
 }
 const chooseCyan = () =>{
     gameFunction(theColorsAvailable[4]);
-        closeColorChoose();
+    closeColorChoose();
 }
 const choosePurple = () =>{
     gameFunction(theColorsAvailable[5]);
-        closeColorChoose();
+    closeColorChoose();
 }
 const chooseGrey = () =>{
     gameFunction(theColorsAvailable[6]);
-        closeColorChoose();
+    closeColorChoose();
 }
 
 
